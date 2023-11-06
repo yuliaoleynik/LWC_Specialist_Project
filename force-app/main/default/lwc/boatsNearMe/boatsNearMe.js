e3e3e78c671e2b6f5cde5b9415ac786f76210970
Yulia <2001yulia2001@gmail.com>
2023-11-06 15:51:45 +0200

    first part of project done

---
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -0,0 +1,75 @@
+import { LightningElement, wire, api } from "lwc";
+import getBoatsByLocation from "@salesforce/apex/BoatDataService.getBoatsByLocation";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+const LABEL_YOU_ARE_HERE = 'You are here!';
+const ICON_STANDARD_USER = 'standard:user';
+const ERROR_TITLE = 'Error loading Boats Near Me';
+const ERROR_VARIANT = 'error';
+
+export default class BoatsNearMe extends LightningElement {
+  
+  @api boatTypeId;
+  mapMarkers = [];
+  isLoading = true;
+  isRendered;
+  latitude;
+  longitude;
+  
+  @wire(getBoatsByLocation, {latitude: '$latitude', longitude: '$longitude', boatTypeId: '$boatTypeId'}) 
+  wiredBoatsJSON({error, data}) 
+  {
+    if(data)
+    {
+        this.createMapMarkers(data);
+    }
+    else if(error)
+    {
+        const toast = new ShowToastEvent({
+            title: ERROR_TITLE,
+            message: error.message,
+            variant: ERROR_VARIANT,
+        });
+        this.dispatchEvent(toast);
+    }
+    this.isLoading = false;
+  }
+  
+  renderedCallback() 
+  {
+    if(!this.isRendered)
+    {
+        this.getLocationFromBrowser();
+    }
+    this.isRendered = true;
+  }
+  
+  getLocationFromBrowser() 
+  {
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(position => {
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+        });
+    }
+  }
+  
+  createMapMarkers(boatData) 
+  {
+      const newMarkers = JSON.parse(boatData).map(boat => {
+            return {
+                title: boat.Name,
+                location: {
+                    Latitude: boat.Geolocation__Latitude__s,
+                    Longitude: boat.Geolocation__Longitude__s
+                }
+            };
+      });
+      newMarkers.unshift({title: LABEL_YOU_ARE_HERE, icon: ICON_STANDARD_USER, location: {
+                Latitude: this.latitude,
+                Longitude: this.longitude
+      }});
+      this.mapMarkers = newMarkers;
+  }
+}
